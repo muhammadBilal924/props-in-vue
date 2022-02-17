54b23aeeace288f21f546a1da7afb84595da57b9
muhammadBilal924 <mbilal60924@gmail.com>
2022-02-17 19:11:47 +0500

    first commit

---
+++ src/App.vue
@@ -0,0 +1,42 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+    <h1>Props in vue js </h1>
+     <Child v-bind:name="usreName" /> 
+  </div>
+</template>
+
+<script>
+// import HelloWorld from './components/HelloWorld.vue'
+ import Child from './components/Child.vue'
+export default {
+  name: 'App',
+  components: {
+    // HelloWorld,
+    Child,
+
+  },
+  data(){
+    return {users:[
+      {name:"Abdullah:Abdullah@test.com"},
+      {name: "Numan:Numan@test.com"},
+      {name: "Sufyan:Sufyan@test.com"},
+      {name: "Zain:Zain@test.com"},
+       {name: "Bilal:Bilal@test.com"}
+      
+    ]}
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Child.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+    <h1>Child Component</h1>
+    <h3>{{name}}</h3>
+    <h3 v-for="user in users" :key="user.name">
+        {{users.name}}
+    </h3>
+    
+
+    
+    </div>
+</template>
+
+<script>
+    export default {
+        name:"Child",
+        props: {
+            name,
+        },
+
+        
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
